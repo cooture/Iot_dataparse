f48293b8d0e3ad72e6cf474aaef43db440495948
Xuebin Ran <ranxuebin@ranxb.cn>
2018-12-26 02:21:41 +0800

    decode message

--- parse/views.py
+++ parse/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
+import execjs
+
+
+
+def get_js():
+    # f = open("D:/WorkSpace/MyWorkSpace/jsdemo/js/des_rsa.js",'r',encoding='UTF-8')
+    f = open("static/decode.js", 'r', encoding='UTF-8')
+    line = f.readline()
+    htmlstr = ''
+    while line:
+        htmlstr = htmlstr + line
+        line = f.readline()
+    return htmlstr
+
 
 def upload(request):
+    jsstr = get_js()
+    ctx = execjs.compile(jsstr)
+    v = ctx.call('decodeMessage', request.GET['data'])
 
-    v = request.GET['data']
     content = {
         'data': v,
     }
     return render(request, 'index.html', content)
-
-
 # Create your views here.
 
 
